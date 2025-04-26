1f52b2001003fedcce841282d001e4fd56a67f57
Kalin G. Lai ; 風凜魂太(KalinKonta) <40635383+KalinLai-void@users.noreply.github.com>
2025-04-26 13:38:22 +0800

    UPDATE: Adding dynamic button generating

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -8,18 +8,27 @@ public class ButtonController : MonoBehaviour
     [SerializeField] private GameObject _btnGroup;
     [SerializeField] private GameObject _btnPrefab;
     [SerializeField] private int _btnNumber = 3;
-    [SerializeField] private Vector2 _pos;
+    [SerializeField] private Vector2 _pos => new Vector2(0, 10);
 
     private List<GameObject> _buttons => new List<GameObject>();
 
     private void Start()
     {
+        // Dynamic generating buttons
         for (int i = 0; i < _btnNumber; i++)
         {
-            _buttons.Add(Instantiate(_btnPrefab, _btnGroup.transform));
-            //_buttons[i].GetComponent<RectTransform>().localPosition
-            //    += new Vector3(i * (_buttons[i].GetComponent<RectTransform>().sizeDelta.x + _pos.x),
-            //                   i * (_buttons[i].GetComponent<RectTransform>().sizeDelta.y + _pos.y), 0);
+            GameObject btn = Instantiate(_btnPrefab, _btnGroup.transform);
+            _buttons.Add(btn);
+            btn.GetComponent<RectTransform>().localPosition 
+                += new Vector3(_pos.x != 0 ? i * (btn.GetComponent<RectTransform>().sizeDelta.x + _pos.x) : 0,
+                               _pos.y != 0 ? i * (btn.GetComponent<RectTransform>().sizeDelta.y + _pos.y) : 0,
+                               0);
+            SetValueToButton();
         }
+    }
+
+    private void SetValueToButton()
+    {
+        // TODO: Input value to ButtonCallback.cs
     }
 }
