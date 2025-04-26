304e4e458a18fb754beefe4b9df36123907fb7be
AssassinYin <yoyooscarsutu@gmail.com>
2025-04-26 14:16:49 +0800

    add missing csv file and json meta file

--- Assets/Scripts/ChoiceManager.cs
+++ Assets/Scripts/ChoiceManager.cs
@@ -2,10 +2,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using Newtonsoft.Json;
+using TMPro;
+using UnityEngine.UI;
 
 public class ChoiceManager : MonoBehaviour
 {
     [SerializeField] private ButtonCallback choice1, choice2, choice3;
+    [SerializeField] private TextMeshProUGUI titleText, descriptionText;
+    [SerializeField] private Image image; 
     
     private const string JsonFileName = "choices-database.json";
     private List<CardData> _cardDatabase;
@@ -13,26 +17,30 @@ public class ChoiceManager : MonoBehaviour
     public void Awake()
     {
         _cardDatabase = JsonConvert.DeserializeObject<List<CardData>>(File.ReadAllText(JsonFileName));
-        foreach (var cardData in _cardDatabase)
-        {
-            Debug.Log(cardData.description + "><" + cardData.title);
-        }
     }
 
     public void OnEnable()
     {
-        
+        choice1.OnClick += OnLoadNewChoice;
+        choice2.OnClick += OnLoadNewChoice;
+        choice3.OnClick += OnLoadNewChoice;
     }
     
     public void OnDisable()
     {
-        
+        choice1.OnClick -= OnLoadNewChoice;
+        choice2.OnClick -= OnLoadNewChoice;
+        choice3.OnClick -= OnLoadNewChoice;
     }
 
-    public void OnLoadNewChoice()
+    private void OnLoadNewChoice(EffectData effectData)
     {
-        choice1.ChoiceData = null;
-        choice2.ChoiceData = null;
-        choice3.ChoiceData = null;
+        
+        image.sprite = Resources.Load<Sprite>("path/to/image_without_extension");
+        
+        var card = _cardDatabase[Random.Range(0, _cardDatabase.Count)];
+        choice1.ChoiceData = card.choices1;
+        choice2.ChoiceData = card.choices2;
+        choice3.ChoiceData = card.choices3;
     } 
 }
