9104cca38554eeea5ccc4650fc23e9451eec94ef
Kalin G. Lai ; 風凜魂太(KalinKonta) <40635383+KalinLai-void@users.noreply.github.com>
2025-04-26 16:32:49 +0800

    UPDATE: Visualizing UI from ValueController.cs

--- Assets/Scripts/ValueController.cs
+++ Assets/Scripts/ValueController.cs
@@ -5,19 +5,52 @@ using UnityEngine.UI;
 
 public class ValueController : MonoBehaviour
 {
-    //public static ValueController instance;
     public EffectData data;
 
-    private void Start()
+    [Header("Max Value Settings")]
+    [SerializeField] private int maxDiscoverability = 100;
+    [SerializeField] private int maxFunds = 100;
+    [SerializeField] private int maxAuthority = 100;
+    [SerializeField] private int maxBelievers = 100;
+
+    [Header("UI")]
+    [SerializeField] private Image discoverabilityImg;
+    [SerializeField] private Image fundsImg;
+    [SerializeField] private Image authorityImg;
+    [SerializeField] private Image believersImg;
+
+    private void Update()
     {
-        //if (instance == null) instance = this;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        discoverabilityImg.fillAmount = (float)data.discoverability / maxDiscoverability;
+        fundsImg.fillAmount = (float)data.funds / maxFunds;
+        authorityImg.fillAmount = (float)data.authority / maxAuthority;
+        believersImg.fillAmount = (float)data.believers / maxBelievers;
     }
 
     public void SetEffectData(EffectData effectData)
     {
         data.funds += effectData.funds;
-        data.discoverability += effectData.discoverability;
         data.authority += effectData.authority;
         data.believers += effectData.believers;
+        data.discoverability += effectData.discoverability;
+        NormalizeValue();
+    }
+
+    private void NormalizeValue()
+    {
+        if (data.funds <= 0) data.funds = 0;
+        if (data.authority <= 0) data.authority = 0;
+        if (data.believers <= 0) data.believers = 0;
+        if (data.discoverability <= 0) data.discoverability = 0;
+
+        if (data.funds >= maxFunds) data.funds = maxFunds;
+        if (data.authority >= maxAuthority) data.authority = maxAuthority;
+        if (data.believers >= maxBelievers) data.believers = maxBelievers;
+        if (data.discoverability >= maxDiscoverability) data.discoverability = maxDiscoverability;
     }
 }
