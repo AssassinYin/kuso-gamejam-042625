8e9ff1bfd538fb7d59e176245ca067ca2b071c5a
Kalin G. Lai ; 風凜魂太(KalinKonta) <40635383+KalinLai-void@users.noreply.github.com>
2025-04-26 13:47:59 +0800

    MERGING: Merging UI and Dynamic generating btn cs

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -8,7 +8,6 @@ public class ButtonController : MonoBehaviour
     [SerializeField] private GameObject _btnGroup;
     [SerializeField] private GameObject _btnPrefab;
     [SerializeField] private int _btnNumber = 3;
-    [SerializeField] private Vector2 _pos => new Vector2(0, 10);
 
     private List<GameObject> _buttons => new List<GameObject>();
 
@@ -19,10 +18,6 @@ public class ButtonController : MonoBehaviour
         {
             GameObject btn = Instantiate(_btnPrefab, _btnGroup.transform);
             _buttons.Add(btn);
-            btn.GetComponent<RectTransform>().localPosition 
-                += new Vector3(_pos.x != 0 ? i * (btn.GetComponent<RectTransform>().sizeDelta.x + _pos.x) : 0,
-                               _pos.y != 0 ? i * (btn.GetComponent<RectTransform>().sizeDelta.y + _pos.y) : 0,
-                               0);
             SetValueToButton();
         }
     }
