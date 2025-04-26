be3191b7a8635fde978d5a21c8b8f7378c76cefb
Kalin G. Lai ; 風凜魂太(KalinKonta) <40635383+KalinLai-void@users.noreply.github.com>
2025-04-26 12:54:59 +0800

    UPDATE: Adding Button Scripts

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject _btnGroup;
+    [SerializeField] private GameObject _btnPrefab;
+    [SerializeField] private int _btnNumber = 3;
+    [SerializeField] private Vector2 _pos;
 
-    // Update is called once per frame
-    void Update()
+    private List<GameObject> _buttons => new List<GameObject>();
+
+    private void Start()
     {
-        
+        for (int i = 0; i < _btnNumber; i++)
+        {
+            _buttons.Add(Instantiate(_btnPrefab, _btnGroup.transform));
+            _buttons[i].GetComponent<RectTransform>().localPosition
+                += new Vector3(i * (_buttons[i].GetComponent<RectTransform>().sizeDelta.x + _pos.x),
+                               i * (_buttons[i].GetComponent<RectTransform>().sizeDelta.y + _pos.y), 0);
+        }
     }
 }
